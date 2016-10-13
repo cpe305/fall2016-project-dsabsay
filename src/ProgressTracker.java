6cf8878678784bc7e4dbb29f615b541a2dfed997
Brady <bradyshutt@gmail.com>
2016-10-13 14:05:04 -0700

    Started storage I/O; not complete

--- src/ProgressTracker.java
+++ src/ProgressTracker.java
@@ -1,19 +1,39 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.io.*;
+
 
 /* Handles reading and writing user progress and statistics */
 public class ProgressTracker {
 	private UserConfiguration settings;
 	
 	//the path where the records will be written to will be stored in the settings object
-	public void savePerformanceRecord(PerformanceRecord record) {
-		
+	public void savePerformanceRecord(PerformanceRecord record) throws IOException {
+      //Needs settings config file to be implemented 
+      //File db = new File(settings.dbfile);
+      try {
+         File db = new File("my-db.db");
+         ObjectOutputStream oos = null; 
+         oos = new ObjectOutputStream(new FileOutputStream(db));
+         oos.writeObject(record);
+         oos.close();
+      } catch(Throwable e) {
+
+      }
 	}
 	
 	//Read all SightSingingRecords from disk into a List
-	public List<SightSingingRecord> getSightSingingRecords() {
-		return new ArrayList<SightSingingRecord>();
+	public SightSingingRecord getSightSingingRecords() throws Throwable {
+      SightSingingRecord ssr = null;
+      InputStream db = new FileInputStream("my-db.db");
+      InputStream buffer = new BufferedInputStream(db);
+      ObjectInputStream input = new ObjectInputStream(buffer);
+      ssr = (SightSingingRecord) input.readObject();
+      System.out.println(ssr);
+      return ssr;
+
+		//return new ArrayList<SightSingingRecord>();
 	}
 	
 	//Read all RhythmRecords from disk into a List
--- src/SightSinger.java
+++ src/SightSinger.java
@@ -1,6 +1,32 @@
+import java.util.Date;
 
 public class SightSinger {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Throwable{
 		System.out.println("SightSinger");
+
+      ProgressTracker pt = new ProgressTracker();
+      PerformanceRecord pr = new SightSingingRecord(
+            2, 
+            "melodytype", 
+            2.4f, 
+            2.1f, 
+            2.2f, 
+            new Date(), 
+            29);
+      try {
+         pt.savePerformanceRecord(pr);
+      } catch (Throwable e) {
+
+      }
+
+
+      PerformanceRecord r = pt.getSightSingingRecords();
+
+      System.out.println(r);
+
+
+
+
+
 	}
 }
