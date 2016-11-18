c403bb63606a4438b3616f8108ee63b94be68843
Daniel Sabsay <dsabsay@calpoly.edu>
2016-11-17 21:10:36 -0800

    Change permissions on install_essentia.sh

--- install_essentia.sh
+++ install_essentia.sh
@@ -9,8 +9,10 @@ sudo apt-get install python-numpy-dev python-numpy
 # navigate to essentia's directory
 cd /home/travis/build/cpe305/fall2016-project-dsabsay/essentia/essentia-releases/essentia-2.1_beta3
 
+# configure
 ./waf configure --mode=release --build-static --with-python --lightweight --fft=KISS
 
+# compile
 ./waf
 
 ./waf install
