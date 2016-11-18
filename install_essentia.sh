9ea566e5941fb3f8231b01ca2a54e07724882ed6
Daniel Sabsay <dsabsay@calpoly.edu>
2016-11-17 21:02:01 -0800

    Add Essentia library, add script to install dependencies for Travis

---
+++ install_essentia.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# install dependencies
+# sudo apt-get install build-essential libfftw3-dev libavcodec-dev libavformat-dev libavutil-dev libavresample-dev python-dev libsamplerate0-dev libtag1-dev
+sudo apt-get install build-essential libavcodec-dev libavformat-dev libavutil-dev libavresample-dev python-dev libsamplerate0-dev libtag1-dev
+
+sudo apt-get install python-numpy-dev python-numpy
+
+# navigate to essentia's directory
+cd /home/travis/build/cpe305/fall2016-project-dsabsay/essentia/essentia-releases/essentia-2.1_beta3
+
+./waf configure --mode=release --build-static --with-python --lightweight --fft=KISS
+
+./waf
+
+./waf install
